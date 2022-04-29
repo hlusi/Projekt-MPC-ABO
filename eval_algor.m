2458a53c9cd3f2d719a05c5096f4f0b2abc66ab8
xrumle01 <91260984+xrumle01@users.noreply.github.com>
2022-04-29 18:06:54 +0200

    Final project - eng comments

--- eval_algor.m
+++ eval_algor.m
@@ -1,14 +1,24 @@
-%
+clear all; close all; clc
+%% Evaluation of the algorithm
 centres_expected = zeros(2,23);
 centres_detected = zeros(2,23);
-error_vec = zeros(1,23);
+errors = zeros(1,23);
 
-
-for i = 1:23
-    centres_expected(:,i) = project2(['data_new/t',num2str(i),'.png']);
-    centres_detected(:,i) = project2(['data_new/',num2str(i),'.png']);
+tic;
+for i = 1:23    % goes through all the images
+    centres_expected(:,i) = project2(['data_new/t',num2str(i),'.png']); % Finding the centre of the references
+    centres_detected(:,i) = project2(['data_new/',num2str(i),'.png']);  % Implemented algorithm
     
-    error_vec(i) = sqrt((centres_expected(1,i)-centres_detected(1,i))^2+(centres_expected(2,i)-centres_detected(2,i))^2);
+    errors(i) = sqrt((centres_expected(1,i)-centres_detected(1,i))^2+(centres_expected(2,i)-centres_detected(2,i))^2); % Vector of errors
 end
+toc;
+%% Mean absolute error
+MAE = sum(errors)/23;
 
-MAE = sum(error_vec)/23;
+%% Visualization
+figure;
+bar(errors)
+line([1 23],[MAE MAE],'color','r','linestyle','-')
+title('MAE – Mean Absolute Error', 'FontSize',20)
+xlabel('# of images','FontSize',15); ylabel('Error [px]','FontSize',15)
+legend('Error',['Average (' num2str(round(MAE,2)) ')'],'FontSize',15,'Location','best')
--- project2.m
+++ project2.m
@@ -1,19 +1,19 @@
 function [C1,C2] = project2(path)
-%nacteni
-%im = im2double(imread('data_new/11.png'));%
+%% Loading of the current image
+%im = im2double(imread('data_new/11.png')); % loading of a specific image
 im = im2double(imread(path));
 
-% Image segmentation
+%% Image segmentation
 seg_im = segmentation(im);
 
-%detekce hran
-im_edge = edge(seg_im,'canny'); % hranova detekce
+%% Edge detection
+im_edge = edge(seg_im,'canny');
 
-%houghova transformace
-rs = 5:35; % rozsah polomeru
+%% Hough transform
+rs = 5:35; % radius range
 hs = zeros(size(im,1),size(im,2),length(rs));
 r_ind =1;
-[x,y]=find(im_edge);  % nalezeni souradnic hranovych pixelu
+[x,y]=find(im_edge);  % finding the coordinates of the edge pixels
 for r=rs
     tmp = gen_kruh(r);
     for i=1:length(x)
@@ -21,51 +21,48 @@ for r=rs
         c2 = y(i);
         if c1>r && c1<size(im,1)-r && c2>r && c2<size(im,2)-r
             hs(c1-r:c1+r,c2-r:c2+r,r_ind)=hs(c1-r:c1+r,c2-r:c2+r,r_ind)+tmp;
-            
         end
     end
    r_ind = r_ind+1;
 end
 
-
-
-%figure (2)
-%subplot 131
-%imshow(im,[])
-%subplot 132
-%imshow(im_edge,[])
-%subplot 133
-%imshow(hs(:,:,1),[])
-
-%pozice stredu
-maximum = max(max(max(hs)));
+%% Finding the position of the centre
+maximum = max(max(max(hs))); % maximum value of the Hough transform
 
 C1 = 0;
 C2 = 0;
 R = 0;
-for i = 1:size(hs,1)
-    for j = 1:size(hs,2)
-        for k = 1:size(hs,3)
+for i = 1:size(hs,1) % rows
+    for j = 1:size(hs,2) % columns
+        for k = 1:size(hs,3) % slices (radii)
             if hs(i,j,k) == maximum
-                C1 = i;
-                C2 = j;
-                R = k;
+                C1 = i; % row position of the centre
+                C2 = j; % column position of the centre
+                R = k;  % slice (radius) of the centre
             end
         end
     end
 end
 
-% figure
+%% Visualization
+% figure;
+% imshow(hs(:,:,R),[])
+% title('Slice of Hough transform which contains the position of the centre (radius = 18 px)', 'FontSize',20)
+
+% figure;
 % subplot 131
 % imshow(im)
+% title('Original image', 'FontSize',20)
 % hold on
-% plot(C2,C1,'r*')
+% plot(C2,C1,'r*','MarkerSize',15,'LineWidth',2)
 % subplot 132
 % imshow(seg_im,[])
+% title('Segmented image', 'FontSize',20)
 % hold on
-% plot(C2,C1,'r*')
+% plot(C2,C1,'r*','MarkerSize',15,'LineWidth',2)
 % subplot 133
 % imshow(im_edge)
+% title('Edge detection', 'FontSize',20)
 % hold on
-% plot(C2,C1,'r*')
+% plot(C2,C1,'r*','MarkerSize',15,'LineWidth',2)
 end
